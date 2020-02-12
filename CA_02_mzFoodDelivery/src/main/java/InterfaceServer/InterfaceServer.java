220405f8ae903b87f69586c07fb2ee8f82cedf37
mohsenfayyaz <mohsen.fayyaz77@gmail.com>
2020-02-12 20:22:17 +0330

    HTTP Request Handler added

--- CA_02_mzFoodDelivery/src/main/java/InterfaceServer/InterfaceServer.java
+++ CA_02_mzFoodDelivery/src/main/java/InterfaceServer/InterfaceServer.java
@@ -1,9 +1,38 @@
 package InterfaceServer;
 
+import HTTPRequestHandler.HTTPRequsestHandler;
+import MzFoodDelivery.MzFoodDelivery;
+import MzFoodDelivery.Restaurant.Restaurant;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import io.javalin.Javalin;
+import java.util.List;
 
 public class InterfaceServer {
+
     public static void main(String[] args) {
+        try {
+            MzFoodDelivery mzFoodDelivery = new MzFoodDelivery();
+
+            importRestaurantsFromWeb(mzFoodDelivery);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void importRestaurantsFromWeb(MzFoodDelivery mzFoodDelivery) throws Exception {
+        final String RESTAURANTS_URI = "http://138.197.181.131:8080/restaurants";
+        String RestaurantsJsonString = HTTPRequsestHandler.getRequest(RESTAURANTS_URI);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        List<Restaurant> restaurants = gson.fromJson(RestaurantsJsonString, new TypeToken<List<Restaurant>>(){}.getType());
+        for (Restaurant restaurant: restaurants) {
+            mzFoodDelivery.addRestaurant(restaurant);
+        }
+    }
+
+    public static void runServer(MzFoodDelivery mzFoodDelivery) throws Exception{
         Javalin app = Javalin.create().start(8080);
         app.get("/", ctx -> ctx.result("Hello World"));
     }
