c3a8c3ff25241316c415f751c822a3c40c3aa229
mohsenfayyaz <mohsen.fayyaz77@gmail.com>
2020-02-12 19:39:56 +0330

    Hello world server is up! :)

--- CA_02_mzFoodDelivery/src/main/java/InterfaceServer/InterfaceServer.java
+++ CA_02_mzFoodDelivery/src/main/java/InterfaceServer/InterfaceServer.java
@@ -1,5 +1,10 @@
 package InterfaceServer;
 
-public class InterfaceServer {
+import io.javalin.Javalin;
 
+public class InterfaceServer {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(8080);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
 }
