99eb357c62302070444439865e8df6e26fc60144
zhivar <zhivarsourati@gmail.com>
2020-03-30 23:50:15 +0430

    Add login page

--- CA_05_mzFoodDelivery/react_app/src/pages/login/Login.js
+++ CA_05_mzFoodDelivery/react_app/src/pages/login/Login.js
@@ -1,5 +1,5 @@
 
-
+import "../../Assets/styles/login-styles.css";
 import * as React from "react";
 
 export default class Login extends React.Component {
@@ -12,8 +12,60 @@ export default class Login extends React.Component {
 
     render() {
         return (
-            <div>
-                login
+            <div className="main-box w-100 h-100">
+                <div className="d-flex flex-row-reverse no-gutters h-100">
+                    <div className="col-lg-9 col-md-8 h-100">
+                        <img className="background-img" src={require("../../Assets/images/food.jpg")} alt="" />
+                    </div>
+                    <div className="col-lg-3 col-md-4 overflow-auto">
+                        <div className="form-container">
+                            <div className="d-flex">
+                                <h1>
+                                    ورود
+                                </h1>
+                            </div>
+                            <div className="d-flex justify-content-center align-items-center ">
+                                <form action="tmp" className=" w-100">
+                                    <div className="">
+                                        <div className="form-item">
+                                            <div className="d-flex">
+                                                <label htmlFor="mail">
+                                                    ایمیل
+                                                </label>
+                                            </div>
+                                            <input type="text" name="mail" id="mail" placeholder="ایمیل" />
+                                        </div>
+                                        <div className="form-item">
+                                            <div className="d-flex">
+                                                <label htmlFor="password">
+                                                    رمز عبور
+                                                </label>
+                                            </div>
+                                            <input type="password" name="password" id="password"
+                                                   placeholder="***********" />
+                                        </div>
+
+                                        <div className="form-submit row">
+                                            <div className="col-auto">
+                                                <input type="submit" value="ورود" />
+                                            </div>
+                                            <div
+                                                className="col-auto d-flex justify-content-center align-items-center clickable">
+                                                <div className="login-arrow">
+                                                    <i className="flaticon-arrow"></i>
+                                                </div>
+                                                <span>
+                                            ثبت‌نام
+                                        </span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
