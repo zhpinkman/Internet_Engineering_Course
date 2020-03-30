978be2d7193a4c9ebe57f35beac3dbcb29f56ad1
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2020-03-30 13:43:20 +0430

    Add Flickity library for carousels

--- CA_05_mzFoodDelivery/react_app/src/App.js
+++ CA_05_mzFoodDelivery/react_app/src/App.js
@@ -1,86 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
 } from "react-router-dom";
 
 import Home from "./pages/home/Home";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>,
-            <Router>
-                <div>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/dashboard">Dashboard</Link>
-                        </li>
-                    </ul>
-
-                    <hr/>
-
-                    {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-                    <Switch>
-                        <Route exact path="/">
-                            <About/>
-                        </Route>
-                        <Route path="/about">
-                            <Home/>
-                        </Route>
-                        <Route path="/dashboard">
-                            <Dashboard/>
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route path="/about">
+                    <About/>
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard/>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
 function About() {
-  return (
-      <div>
-        <h2>About</h2>
-      </div>
-  );
+    return (
+        <div>
+            <h2>About</h2>
+        </div>
+    );
 }
 
 function Dashboard() {
-  return (
-      <div>
-        <h2>Dashboard</h2>
-      </div>
-  );
+    return (
+        <div>
+            <h2>Dashboard</h2>
+        </div>
+    );
 }
 
 export default App;
