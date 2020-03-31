e73e7ff639e5b15d316b2d36876a5f2bde8186aa
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2020-03-31 16:08:07 +0430

    Start food party scheduler at server start

--- CA_05_mzFoodDelivery/server/CA_05_mzFoodDelivery/src/main/java/ir/ac/ut/ie/CA_05_mzFoodDelivery/Ca05MzFoodDeliveryApplication.java
+++ CA_05_mzFoodDelivery/server/CA_05_mzFoodDelivery/src/main/java/ir/ac/ut/ie/CA_05_mzFoodDelivery/Ca05MzFoodDeliveryApplication.java
@@ -2,16 +2,23 @@ package ir.ac.ut.ie.CA_05_mzFoodDelivery;
 
 import ir.ac.ut.ie.CA_05_mzFoodDelivery.domain.MzFoodDelivery.MzFoodDelivery;
 import ir.ac.ut.ie.CA_05_mzFoodDelivery.services.RestaurantsService;
+import ir.ac.ut.ie.CA_05_mzFoodDelivery.utils.schedulers.ScheduledParty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class Ca05MzFoodDeliveryApplication {
-
 	public static void main(String[] args) {
+		ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 		try {
 			RestaurantsService.getInstance().importRestaurantsFromWeb();
 			MzFoodDelivery.getInstance().importFoodPartyFromWeb();
+			new ScheduledParty().run();
+			scheduler.scheduleAtFixedRate(new ScheduledParty(), 15, 15, TimeUnit.SECONDS);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
