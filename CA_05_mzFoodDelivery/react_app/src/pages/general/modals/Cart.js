fe1e8df556001ff414d3c0fbde7a7f0734e9ccce
zhivar <zhivarsourati@gmail.com>
2020-04-01 21:49:27 +0430

    Add user cart data

--- CA_05_mzFoodDelivery/react_app/src/pages/general/modals/Cart.js
+++ CA_05_mzFoodDelivery/react_app/src/pages/general/modals/Cart.js
@@ -1,71 +1,96 @@
 import * as React from "react";
 import "../../../Assets/styles/cart-styles.css";
+import UserService from "../../../services/UserService";
+import {enToFaNumber} from "../../../utils/utils";
 
 
 export default class Cart extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            cart: {}
+        }
+    }
+
+
+    componentDidMount() {
+        UserService.getCart().then(cart => {
+            this.setState({cart: cart.data});
+            console.log(this.state.cart);
+        })
     }
 
 
     render() {
         return (
             <div>
-                <div className="cart-container">
+                <div className="card cart-container">
+                    {this.state.cart && this.state.cart.cartItems && (
                         <span className="cart-title">
                             سبد خرید
                         </span>
-
-                    <div className="cart-list-wrapper d-flex justify-content-center">
-
-                        <div className="cart-list-item-wrapper d-flex flex-column">
-                            <div className="d-flex flex-row justify-space-between">
-                                <div className="align-self-start">
-                                    پیتزا اعلا
-                                </div>
-                                <div className="align-self-end mr-auto">
-                                    <a><i className="glyph-icon flaticon-minus cart-icon"></i></a>
-                                    <span className="p-1">۳۰</span>
-                                    <a><i className="glyph-icon flaticon-plus cart-icon"></i></a>
-                                </div>
-                            </div>
-                            <div className="price d-flex flex-row justify-content-end">
-                                ۳۹۰۰۰ تومان
-                            </div>
-                        </div>
-                        <div className="break"></div>
-                        <div className="cart-list-item-wrapper d-flex flex-column">
-                            <div className="d-flex flex-row justify-space-between">
-                                <div className="align-self-start">
-                                    پیتزا نیمه اعلا
-                                </div>
-                                <div className="align-self-end mr-auto">
-                                    <a><i className="glyph-icon flaticon-minus cart-icon"></i></a>
-                                    <span className="p-1">۳۰</span>
-                                    <a><i className="glyph-icon flaticon-plus cart-icon"></i></a>
-                                </div>
-                            </div>
-                            <div className="price d-flex flex-row justify-content-end">
-                                ۳۹۰۰۰ تومان
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="total-price">
-                        جمع کل:
-                        <span>
-                                ۳۹۰۰۰ تومان
-                            </span>
-                    </div>
-
-                    <button type="button" className="btn btn-default btn-cyan">
-                        تایید نهایی
-                    </button>
-
-
+                    ) }
+                    {this.getCart()}
                 </div>
             </div>
         );
     }
+
+    getCart() {
+        if (this.state.cart && this.state.cart.cartItems)
+        return (
+        <>
+            <div className="cart-list-wrapper d-flex justify-content-center">
+                        {this.state.cart.cartItems.map(cartItem => {
+                            return (
+                                <>
+                                <div className="cart-list-item-wrapper d-flex flex-column">
+                                    <div className="d-flex flex-row justify-space-between">
+                                        <div className="align-self-start">
+                                            {cartItem.food.name}
+                                        </div>
+                                        <div className="align-self-end mr-auto">
+                                            <a><i className="glyph-icon flaticon-minus cart-icon"></i></a>
+                                            <span className="p-1">
+                                                {cartItem.quantity}
+                                            </span>
+                                            <a><i className="glyph-icon flaticon-plus cart-icon"></i></a>
+                                        </div>
+                                    </div>
+                                    <div className="price d-flex flex-row justify-content-end">
+                                        {enToFaNumber(cartItem.food.price * cartItem.quantity)}
+                                        تومان
+                                    </div>
+                                </div>
+                                <div className="break"></div>
+                                </>
+                            )
+                        })}
+
+            </div>
+            <div className="total-price">
+                جمع کل:
+                <span>
+                    {enToFaNumber(this.state.cart.totalPrice || 0)}
+                تومان
+                            </span>
+            </div>
+
+            <button type="button" className="btn btn-default btn-cyan">
+                تایید نهایی
+            </button>
+
+        </>
+        );
+        else
+            return (
+            <div className="spinner-grow text-danger food-party-loading-box align-self-center" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+            )
+    }
+
 }
+
+
--- CA_05_mzFoodDelivery/react_app/src/pages/restaurant/Restaurant.js
+++ CA_05_mzFoodDelivery/react_app/src/pages/restaurant/Restaurant.js
@@ -6,6 +6,7 @@ import {FoodPartyService} from "../../services/FoodPartyService";
 import RestaurantService from "../../services/RestaurantService";
 import FoodPartyCard from "../home/foodParty/foodPartyCard/FoodPartyCard";
 import RestaurantMenuCard from "./RestaurantMenuCard";
+import Cart from "../general/modals/Cart";
 
 export default class Restaurant extends React.Component {
     constructor(props) {
@@ -112,59 +113,7 @@ export default class Restaurant extends React.Component {
                     <div className="row mt-5">
 
                         <div className="col-lg-4 col-6">
-                            <div className="card cart-container">
-                        <span className="cart-title">
-                            سبد خرید
-                        </span>
-
-                                <div className="cart-list-wrapper d-flex justify-content-center">
-
-                                    <div className="cart-list-item-wrapper d-flex flex-column">
-                                        <div className="d-flex flex-row justify-space-between">
-                                            <div className="align-self-start">
-                                                پیتزا اعلا
-                                            </div>
-                                            <div className="align-self-end mr-auto">
-                                                <a><i className="glyph-icon flaticon-minus cart-icon"></i></a>
-                                                <span className="p-1">۳۰</span>
-                                                <a><i className="glyph-icon flaticon-plus cart-icon"></i></a>
-                                            </div>
-                                        </div>
-                                        <div className="price d-flex flex-row justify-content-end">
-                                            ۳۹۰۰۰ تومان
-                                        </div>
-                                    </div>
-                                    <div className="break"></div>
-                                    <div className="cart-list-item-wrapper d-flex flex-column">
-                                        <div className="d-flex flex-row justify-space-between">
-                                            <div className="align-self-start">
-                                                پیتزا نیمه اعلا
-                                            </div>
-                                            <div className="align-self-end mr-auto">
-                                                <a><i className="glyph-icon flaticon-minus cart-icon"></i></a>
-                                                <span className="p-1">۳۰</span>
-                                                <a><i className="glyph-icon flaticon-plus cart-icon"></i></a>
-                                            </div>
-                                        </div>
-                                        <div className="price d-flex flex-row justify-content-end">
-                                            ۳۹۰۰۰ تومان
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div className="total-price">
-                                    جمع کل:
-                                    <span>
-                                ۳۹۰۰۰ تومان
-                            </span>
-                                </div>
-
-                                <button type="button" className="btn btn-default btn-cyan">
-                                    تایید نهایی
-                                </button>
-
-
-                            </div>
+                            <Cart/>
                         </div>
 
                         <div className="col-lg-8 col-6 food-list-container">
