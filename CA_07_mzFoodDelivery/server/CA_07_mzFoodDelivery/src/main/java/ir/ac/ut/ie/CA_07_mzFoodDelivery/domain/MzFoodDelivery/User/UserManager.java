6f4f73da6b368384a4748f1c2cb51306712096fb
zhivar <zhivarsourati@gmail.com>
2020-05-26 20:42:02 +0430

    Add password check in previous phase

--- CA_07_mzFoodDelivery/server/CA_07_mzFoodDelivery/src/main/java/ir/ac/ut/ie/CA_07_mzFoodDelivery/domain/MzFoodDelivery/User/UserManager.java
+++ CA_07_mzFoodDelivery/server/CA_07_mzFoodDelivery/src/main/java/ir/ac/ut/ie/CA_07_mzFoodDelivery/domain/MzFoodDelivery/User/UserManager.java
@@ -8,119 +8,136 @@ import ir.ac.ut.ie.CA_07_mzFoodDelivery.domain.MzFoodDelivery.Delivery.Order;
 import ir.ac.ut.ie.CA_07_mzFoodDelivery.domain.MzFoodDelivery.Restaurant.Food;
 import ir.ac.ut.ie.CA_07_mzFoodDelivery.domain.MzFoodDelivery.Restaurant.Location;
 import ir.ac.ut.ie.CA_07_mzFoodDelivery.repository.MzRepository;
-import org.springframework.security.core.context.SecurityContextHolder;
-
 import java.sql.SQLException;
 import java.util.List;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 public class UserManager {
 
+  public UserManager() {
+    //        System.out.println("mohsen here");
+    //        User user = new User("Ehsan", "Khames", "ekhamespanah@yahoo.com",
+    //        "1234","989123456789", new Location(0, 0), 0, 0); try {
+    //            MzRepository.getInstance().insertUser(user);
+    ////            System.out.println("zhivar here");
+    //        } catch (SQLException ignored) {}
+  }
 
+  public void addToCart(CartItem cartItem) throws Exception {
+    User user = MzRepository.getInstance().getUser(
+        SecurityContextHolder.getContext().getAuthentication().getName());
+    user.addToCart(cartItem);
+  }
 
-    public UserManager() {
-//        System.out.println("mohsen here");
-//        User user = new User("Ehsan", "Khames", "ekhamespanah@yahoo.com", "1234","989123456789", new Location(0, 0), 0, 0);
-//        try {
-//            MzRepository.getInstance().insertUser(user);
-////            System.out.println("zhivar here");
-//        } catch (SQLException ignored) {}
+  public Location getLocation(String email) throws SQLException {
+    return MzRepository.getInstance().getUser(email).getLocation();
+  }
+
+  public List<CartItem> getCart() throws SQLException {
+    User user = MzRepository.getInstance().getUser(
+        SecurityContextHolder.getContext().getAuthentication().getName());
+    return user.getUserCart();
+  }
+
+  public List<CartItem> getAllCarts() throws SQLException {
+    return MzRepository.getInstance().getAllCarts();
+  }
+
+  public String getBriefCartJson() throws SQLException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    JsonElement cartJsonElement = gson.toJsonTree(this.getCart());
+    JsonArray array =
+        cartJsonElement.getAsJsonObject().get("cartItems").getAsJsonArray();
+    array.forEach(item -> {
+      item.getAsJsonObject().addProperty("foodName", item.getAsJsonObject()
+                                                         .get("food")
+                                                         .getAsJsonObject()
+                                                         .get("name")
+                                                         .getAsString());
+      item.getAsJsonObject().remove("food");
+      item.getAsJsonObject().remove("restaurant");
+    });
+    return gson.toJson(cartJsonElement);
+  }
+
+  public List<CartItem> finalizeOrder() throws Exception {
+    User user = MzRepository.getInstance().getUser(
+        SecurityContextHolder.getContext().getAuthentication().getName());
+    double totalPrice = user.getCartTotalPrice();
+    if (user.getUserCartSize() == 0)
+      throw new Exception("user cart is empty");
+    if (totalPrice > user.getCredit())
+      throw new Exception("credit is not enough for finalizing your order");
+    if (foodRepoEmpty(user.getUserCart()))
+      throw new Exception(
+          "count of this offer is not enough for you to submit");
+    user.withdrawCredit(totalPrice);
+    return user.finalizeOrder();
+  }
+
+  public boolean foodRepoEmpty(List<CartItem> cartItems) throws SQLException {
+    for (CartItem cartItem : cartItems) {
+      Food food = MzRepository.getInstance().getFood(cartItem.getRestaurantId(),
+                                                     cartItem.getFoodName());
+      if (!food.hasEnoughAmount(cartItem.getQuantity()))
+        return true;
     }
+    return false;
+  }
 
-    public void addToCart(CartItem cartItem) throws Exception {
-        User user = MzRepository.getInstance().getUser(SecurityContextHolder.getContext().getAuthentication().getName());
-        user.addToCart(cartItem);
-    }
+  public int getUserCartSize() throws SQLException {
+    User user = MzRepository.getInstance().getUser(
+        SecurityContextHolder.getContext().getAuthentication().getName());
+    return user.getUserCartSize();
+  }
 
-    public Location getLocation(String email) throws SQLException{
-        return MzRepository.getInstance().getUser(email).getLocation();
-    }
+  public User getUser(String email) throws SQLException {
+    return MzRepository.getInstance().getUser(email);
+  }
 
-    public List<CartItem> getCart() throws SQLException {
-        User user = MzRepository.getInstance().getUser(SecurityContextHolder.getContext().getAuthentication().getName());
-        return user.getUserCart();
-    }
+  public void chargeUserCredit(double amount) throws Exception {
+    User user = MzRepository.getInstance().getUser(
+        SecurityContextHolder.getContext().getAuthentication().getName());
+    user.chargeUserCredit(amount);
+  }
 
-    public List<CartItem> getAllCarts() throws SQLException {
-        return MzRepository.getInstance().getAllCarts();
-    }
+  public void deleteFromCart(String restaurantId, String foodName)
+      throws Exception {
+    User user = MzRepository.getInstance().getUser(
+        SecurityContextHolder.getContext().getAuthentication().getName());
+    user.deleteFromCart(restaurantId, foodName);
+  }
 
-    public String getBriefCartJson() throws SQLException {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        JsonElement cartJsonElement = gson.toJsonTree(this.getCart());
-        JsonArray array = cartJsonElement.getAsJsonObject().get("cartItems").getAsJsonArray();
-        array.forEach(item -> {
-            item.getAsJsonObject().addProperty("foodName", item.getAsJsonObject().get("food").getAsJsonObject().get("name").getAsString());
-            item.getAsJsonObject().remove("food");
-            item.getAsJsonObject().remove("restaurant");
-        });
-        return gson.toJson(cartJsonElement);
-    }
+  public Order getLatestOrder() throws SQLException {
+    List<Order> orders = MzRepository.getInstance().getAllOrders();
+    return orders.get(orders.size() - 1);
+  }
 
-    public List<CartItem> finalizeOrder() throws Exception {
-        User user = MzRepository.getInstance().getUser(SecurityContextHolder.getContext().getAuthentication().getName());
-        double totalPrice = user.getCartTotalPrice();
-        if (user.getUserCartSize() == 0)
-            throw new Exception("user cart is empty");
-        if (totalPrice > user.getCredit())
-            throw new Exception("credit is not enough for finalizing your order");
-        if (foodRepoEmpty(user.getUserCart()))
-            throw new Exception("count of this offer is not enough for you to submit");
-        user.withdrawCredit(totalPrice);
-        return user.finalizeOrder();
-    }
+  public List<Order> getOrders() throws SQLException {
+    return MzRepository.getInstance().getOrders(
+        SecurityContextHolder.getContext().getAuthentication().getName());
+  }
 
-    public boolean foodRepoEmpty(List<CartItem> cartItems) throws SQLException {
-        for (CartItem cartItem : cartItems) {
-            Food food = MzRepository.getInstance().getFood(cartItem.getRestaurantId(), cartItem.getFoodName());
-            if (!food.hasEnoughAmount(cartItem.getQuantity()))
-                return true;
-        }
-        return false;
+  public void addUser(String email, String firstName, String lastName,
+                      String password) throws Exception {
+    try {
+      User user = new User(firstName, lastName, email, password, "",
+                           new Location(0, 0), 0, 0);
+      MzRepository.getInstance().insertUser(user);
+    } catch (SQLException e) {
+      throw new Exception("email is already taken");
     }
+  }
 
-    public int getUserCartSize() throws SQLException {
-        User user = MzRepository.getInstance().getUser(SecurityContextHolder.getContext().getAuthentication().getName());
-        return user.getUserCartSize();
+  public User loginUser(String email, String password) throws Exception {
+    try {
+      User user = MzRepository.getInstance().getUser(email);
+      if (user.getPassword().equals(password))
+        return user;
+      else
+        throw new Exception("passwords does not match");
+    } catch (SQLException e) {
+      throw new Exception("user not available");
     }
-
-    public User getUser(String email) throws SQLException{
-        return MzRepository.getInstance().getUser(email);
-    }
-
-    public void chargeUserCredit(double amount) throws Exception {
-        User user = MzRepository.getInstance().getUser(SecurityContextHolder.getContext().getAuthentication().getName());
-        user.chargeUserCredit(amount);
-    }
-
-    public void deleteFromCart(String restaurantId, String foodName) throws Exception {
-        User user = MzRepository.getInstance().getUser(SecurityContextHolder.getContext().getAuthentication().getName());
-        user.deleteFromCart(restaurantId, foodName);
-    }
-
-    public Order getLatestOrder() throws SQLException {
-        List<Order> orders = MzRepository.getInstance().getAllOrders();
-        return orders.get(orders.size() - 1);
-    }
-
-    public List<Order> getOrders() throws SQLException {
-        return MzRepository.getInstance().getOrders(SecurityContextHolder.getContext().getAuthentication().getName());
-    }
-
-    public void addUser(String email, String firstName, String lastName, String password) throws Exception {
-        try {
-            User user = new User(firstName, lastName, email, password, "", new Location(0, 0), 0, 0);
-            MzRepository.getInstance().insertUser(user);
-        } catch (SQLException e) {
-            throw new Exception("email is already taken");
-        }
-    }
-
-    public User loginUser(String email, String password) throws Exception {
-        try {
-            User user = MzRepository.getInstance().getUser(email);
-            return user;
-        } catch (SQLException e) {
-            throw new Exception("user not available");
-        }
-    }
+  }
 }
